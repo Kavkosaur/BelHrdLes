6320d3ea98b93ba94688637b5858eb91d3661091
Kavkosaur <kavuch14@gmail.com>
2019-10-15 20:59:07 +0300

    ForFunAGAIN

--- src/by/belhard/j18/les2/Les2.java
+++ src/by/belhard/j18/les2/Les2.java
@@ -1,7 +1,18 @@
 package by.belhard.j18.les2;
+//hz zochem no norm
 
 public class Les2 {
     public static void main(String[] args) {
+        byte a = 0;
+        for (int i = 0; i < 129; i++) {
+            byte b = a;
+            a += i;
+            if (b < a) {
+                System.out.println("Let's see:" + a);
+            } else {
+                System.out.println("Oh,shield, an overload >_< :" + a);
+            }
 
+        }
     }
 }

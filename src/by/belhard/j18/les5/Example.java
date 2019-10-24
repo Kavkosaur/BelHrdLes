08f56562f5715ecae8854a7961cb4f707f4ecb46
Kavkosaur <kavuch14@gmail.com>
2019-10-24 21:14:43 +0300

    Ohhhh...

---
+++ src/by/belhard/j18/les5/Example.java
@@ -0,0 +1,12 @@
+package by.belhard.j18.les5;
+
+public class Example {
+
+   private static int counter = 0;
+   private String str;
+
+
+
+    }
+
+}
---
+++ src/by/belhard/j18/les5/Main.java
@@ -0,0 +1,9 @@
+package by.belhard.j18.les5;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+}
